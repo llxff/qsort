a9bf5ea46e3eba4bb60e779d064b160a7d620254
Александр Фомин <ll.wg.bin@gmail.com>
2016-11-28 22:10:27 +0300

    array length

--- src/qsort.h
+++ src/qsort.h
@@ -1,4 +1,6 @@
 #ifndef TASK_QSORT_H
 #define TASK_QSORT_H
 
+#define length(x) (sizeof(x) / sizeof((x)[0]))
+
 #endif
--- src/test.c
+++ src/test.c
@@ -3,13 +3,20 @@
 #include "expect.h"
 #include "qsort.h"
 
-void test_qsort() {
-  expect_true("should be true", 1);
+void test_empty_array_length() {
+  int array[] = {};
+  expect_true("should return 0 if array is empty", length(array) == 0);
+}
+
+void test_length_of_array_with_elements() {
+  int array[] = {1, 2, 3, 4};
+  expect_true("should return array length", length(array) == 4);
 }
 
 int main(int argc, char *argv[])
 {
-  test_qsort();
+  test_empty_array_length();
+  test_length_of_array_with_elements();
 
   return tests_result_code();
 }
